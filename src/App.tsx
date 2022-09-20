5925e889b9fc3a8518ea884a31cb53df0b19ed6b
Ryan Lewis <rlewis94@gmail.com>
2022-09-19 23:55:17 -0700

    fixed edit functionality

--- src/App.tsx
+++ src/App.tsx
@@ -31,43 +31,103 @@ function App() {
   const EntryItem: React.FC<EntryItemProps> = ({ entry }) => {
     // code for each of the job entries
     const [isEditing, setIsEditing] = useState(false);
+
+    const [job, setJob] = useState(entry.job);
+    const [location, setLocation] = useState(entry.location);
+    const [startDate, setStartDate] = useState(entry.startDate);
+    const [endDate, setEndDate] = useState(entry.endDate);
+    const [responsibilities, setResponsibilities] = useState(entry.responsibilities);
+    const [tools, setTools] = useState(entry.tools);
+
+    function submitEdit(
+      oldJobName:string,
+      job:string,
+      location:string,
+      startDate:string,
+      endDate:string,
+      responsibilities:string,
+      tools:string ) {
+      setEntries(
+        [...entries.filter((e: Entry) => {return e.job !== oldJobName}),
+          {job ,location,startDate,endDate,responsibilities,tools} // new entry
+        ]
+      );
+    }
+
     return isEditing ? (
       // isEditing === True
-      <div className='job' style={{color:'red'}}>
-        <div className='row'>
-          <div className='jobtitle col'>{entry.job}</div>
-          <div className='col-auto'/>
-          <div className='col icons'>
-            {/* could be prettier */}
-            <button
-              type="submit"
-              onClick={(e) => {
-                e.preventDefault();
-                setIsEditing(false)
-              }}
-            >
-              <img className='icon' src='./edit.svg'/>
-            </button>
-            <button
-              type="submit"
-              onClick={(e) => {
-                e.preventDefault();
-                setEntries(
-                  entries.filter((e: Entry) => {
-                    return e.job !== entry.job
-                  })
-                );
-              }}
-            >
-              <img className='icon trashicon' src='./trash.svg'/>
-            </button>
-          </div>
-        </div>
-        <div>{entry.location}</div>
-        <div>{entry.startDate} - {entry.endDate}</div>
-        <div>Responsibilities: {entry.responsibilities}</div>
-        <div>Tools: {entry.tools}</div>
-      </div>
+      <div className='job'>
+        <form>
+        Job:
+        <input
+          type="text"
+          value={job}
+          onChange={(e) =>{
+            setJob(e.target.value)
+          }}
+        />
+        <br/>
+        Location:
+        <input
+          type="text"
+          value={location}
+          onChange={(e) =>{
+            setLocation(e.target.value)
+          }}
+        />
+        <br/>
+        Start Date:
+        <input
+          type="text"
+          value={startDate}
+          onChange={(e) =>{
+            setStartDate(e.target.value)
+          }}
+        />
+        End Date:
+        <input
+          type="text"
+          value={endDate}
+          onChange={(e) =>{
+            setEndDate(e.target.value)
+          }}
+        />
+        <br/>
+        Responsibilities:
+        <input
+          type="text"
+          value={responsibilities}
+          onChange={(e) =>{
+            setResponsibilities(e.target.value)
+          }}
+        />
+        <br/>
+        Tools:
+        <input
+          type="text"
+          value={tools}
+          onChange={(e) =>{
+            setTools(e.target.value)
+          }}
+        />
+        <button
+        type="submit"
+          onClick={(e) => {
+            e.preventDefault() // this makes sure it doesn't handle like a normal button and refresh the page
+            submitEdit(
+              entry.job,
+              job,
+              location,
+              startDate,
+              endDate,
+              responsibilities,
+              tools)
+          }}
+        >
+        Update
+        </button>
+      </form>
+    </div>
     ) : (
       // isEditing === false
       <div className='job'>
@@ -89,7 +149,7 @@ function App() {
               type="submit"
               onClick={(e) => {
                 e.preventDefault();
-                setEntries(
+                setEntries( // remove the entry using filter
                   entries.filter((e: Entry) => {
                     return e.job !== entry.job
                   })
